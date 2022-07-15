151dad9a7feab45e0fd441836c6ac81bee11a221
Matthew Pagan <mastapegs01@gmail.com>
2022-07-14 20:48:55 -0400

    feat: binary tree for sorting logs

--- src/CourseTwo/LogAnalysis.hs
+++ src/CourseTwo/LogAnalysis.hs
@@ -3,10 +3,6 @@
 module CourseTwo.LogAnalysis where
 
 import CourseTwo.Deps.Log
-  ( LogMessage (..),
-    MessageType (Error, Info, Warning),
-    testParse,
-  )
 import Data.Foldable (for_)
 
 parse :: String -> [LogMessage]
@@ -25,13 +21,30 @@ parseMessage string = case words string of
 runParserWithLines :: Int -> IO [LogMessage]
 runParserWithLines n = testParse parse n "src/CourseTwo/Deps/error.log"
 
-printResultsWithLines :: Int -> IO ()
-printResultsWithLines n = do
+printLines :: Int -> IO ()
+printLines n = do
   logMessages <- runParserWithLines n
   for_ logMessages print
 
-printLines :: Int -> IO ()
-printLines = printResultsWithLines
-
 printAllLines :: IO ()
 printAllLines = printLines 6000
+
+--
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert logMessage Leaf = Node Leaf logMessage Leaf
+insert _ tree@(Node _ (Unknown _) _) = tree
+insert
+  logMessage@(LogMessage _ stampInInsert _)
+  (Node leftTree (LogMessage _ stampInTree _) rightTree) =
+    if stampInInsert <= stampInTree
+      then insert logMessage leftTree
+      else insert logMessage rightTree
+
+build :: [LogMessage] -> MessageTree
+build logMessages = buildHelper logMessages Leaf
+  where
+    buildHelper :: [LogMessage] -> MessageTree -> MessageTree
+    buildHelper [] tree = tree
+    buildHelper (x : xs) tree = buildHelper xs (insert x tree)
